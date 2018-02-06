7092902b2cd53f7d1817e2f8fbc136d9b44064a2
BenoitMorel <benomorel@gmail.com>
2018-02-06 11:40:59 +0100

    Add a script to run multiraxml on the cluster

---
+++ scripts/multi-raxml/haswellrun.py
@@ -0,0 +1,31 @@
+import os
+import sys
+sys.path.insert(0, 'scripts')
+import experiments as exp
+sys.path.insert(0, os.path.join(exp.multiraxml_root, "scripts"))
+import cluster
+
+datadir = os.path.join(exp.datasets_root, "multi-raxml", "commands")
+
+if (len(sys.argv) != 3):
+  datasets = os.listdir(datadir)
+  print("Syntax : python scripts/haswellrun.py command_dir threads_number")
+  print("Suggestions of datasets: ")
+  print('\n'.join(datasets))
+  sys.exit(0)
+
+data = sys.argv[1]
+threads = int(sys.argv[2])
+
+resultsdir = os.path.join(exp.results_root, "multi-raxml", data)
+os.makedirs(resultsdir, exist_ok=True)
+result_msg = "multi-raxml git: \n" + exp.get_git_info(exp.multiraxml_root)
+exp.write_results_info(resultsdir, result_msg) 
+
+command = os.path.join(datadir, data, "command.txt")
+warning = os.path.join(datadir, data, "warning.txt")
+exp.display_warning_file(warning)
+
+cluster.duplicateAndRun(command, threads, resultsdir)
+
+
