c2b7eb62edd3a71d814c8883b97f56c071444ff1
BenoitMorel <benomorel@gmail.com>
2018-02-12 15:35:12 +0100

    move two useful scripts from treerecs to this repo

---
+++ scripts/treerecs/phyldog_to_treerecs_map.py
@@ -0,0 +1,17 @@
+import sys
+
+def convert(phyldog_filename, treerecs_filename):
+  ilines = open(phyldog_filename).readlines()
+  with open(treerecs_filename, "w") as f:
+    for line in ilines:
+      split = line[:-1].split(":")
+      species = split[0]
+      genes = split[1].split(";")
+      for gene in genes:
+        f.write(gene + " " + species + "\n")
+
+if (3 != len(sys.argv)):
+  print("Syntax: python phyldogToTreerecsMap.py phyldog_filename treerecs_filename")
+  sys.exit()
+convert(sys.argv[1], sys.argv[2])
+
