fd43611363a070743865cb761432b74e58b45ae1
BenoitMorel <benomorel@gmail.com>
2018-03-14 10:21:54 +0100

    Add bootstraps options to the multi-raxml experiments

--- scripts/multi-raxml/fromfastadir.py
+++ scripts/multi-raxml/fromfastadir.py
@@ -12,7 +12,7 @@ datas["sub_ensembl_1000"] = os.path.join(exp.bigdatasets_root, "ensembl_1000_15/
 
 
 def print_help():
-  print("syntax: python fromfastadir_normal.py data cluster_mode ranks ")
+  print("syntax: python fromfastadir_normal.py data cluster_mode bs_numbers ranks ")
   print("  possible datas: ")
   for data in datas:
     print("    " + data)
@@ -22,14 +22,15 @@ def print_help():
 
 
 
-if ((len(sys.argv) != 4) or (sys.argv[1] not in datas)):
+if ((len(sys.argv) != 5) or (sys.argv[1] not in datas)):
   print("Error! Syntax should be " )
   print_help()
   sys.exit(0)
 
 data = sys.argv[1]
 cluster_mode = sys.argv[2]
-ranks = sys.argv[3]
+bootstraps_number = int(sys.argv[3])
+ranks = sys.argv[4]
 
 isHaswell = (cluster_mode == "haswell")
 implementation = "--split_scheduler"
@@ -38,7 +39,11 @@ raxmlbin = os.path.join(exp.raxml_root, "bin")
 options = os.path.join(exp.multiraxml_root, "examples", "raxml_options.txt")
 
 fastafiles = datas[data]
-resultsdir = os.path.join(exp.results_root, "multi-raxml", "fromfastadir", cluster_mode + "_" + ranks, data)
+datakey = data
+resultsdir = os.path.join(exp.results_root, "multi-raxml")
+if (bootstraps_number != 0):
+  resultsdir = os.path.join(resultsdir, "bootstraps_" + str(bootstraps_number))
+resultsdir = os.path.join(resultsdir, cluster_mode + "_" + ranks, data)
 resultsdir = exp.create_result_dir(resultsdir)
 result_msg = "multi-raxml git: \n" + exp.get_git_info(exp.multiraxml_root) + "\n"
 result_msg += "raxml git: \n" + exp.get_git_info(exp.raxml_root) + "\n"
@@ -52,6 +57,7 @@ command.append(raxmlbin)
 command.append(fastafiles)
 command.append(resultsdir)
 command.append(options)
+command.append(str(bootstraps_number))
 command.append(ranks)
 
 if (isHaswell):
