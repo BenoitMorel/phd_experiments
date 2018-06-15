2698ab1b5d2046043fa0d1950e60a5c6a70019af
BenoitMorel <benomorel@gmail.com>
2018-06-15 13:13:29 +0200

    Version the most relevant and successful pargenes experiments

---
+++ results/multi-raxml/archive/ensembl/no_bootstraps_--scheduler_onecore/haswell_512/run_0/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -o /hits/basement/sco/morel/github/phd_experiments/results/multi-raxml/ensembl/no_bootstraps_--scheduler_onecore/haswell_512/run_0/logs.out
+#SBATCH -B 2:8:1
+#SBATCH -N 32
+#SBATCH -n 512
+#SBATCH --threads-per-core=1
+#SBATCH --cpus-per-task=1
+#SBATCH --hint=compute_bound
+#SBATCH -t 24:00:00
+
+python3 /hits/basement/sco/morel/github/phd_experiments/../multi-raxml/multi-raxml/multi-raxml.py -a /hits/basement/sco/morel/github/phd_experiments/../datasets/ensembl_8880_15/fasta_files/ -o /hits/basement/sco/morel/github/phd_experiments/results/multi-raxml/ensembl/no_bootstraps_--scheduler_onecore/haswell_512/run_0/multiraxml_run -r /hits/basement/sco/morel/github/phd_experiments/datasets/multi-raxml/option_files/raxml_global_options.txt -b 0 -c 512 --scheduler onecore
