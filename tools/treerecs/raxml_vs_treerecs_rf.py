245a2cd92161eb8837c01e852cc7586d0d27a42e
Benoit Morel <morelbt@hitssv543.villa-bosch.de>
2018-09-07 14:31:22 +0200

    Improve the treerecs vs raxml experiment script to compare results with and without tree search

--- tools/treerecs/raxml_vs_treerecs_rf.py
+++ tools/treerecs/raxml_vs_treerecs_rf.py
@@ -29,12 +29,15 @@ def get_relative_rf(tree1, tree2):
   return float(rf[0]) / float(rf[1])
 
 def read_list_trees(newick):
-  trees = []
-  for line in open(newick).readlines():
-    if (line.startswith(">")):
-      continue
-    trees.append(Tree(line, format=1))
-  return trees
+  try:
+    trees = []
+    for line in open(newick).readlines():
+      if (line.startswith(">")):
+        continue
+      trees.append(Tree(line, format=1))
+    return trees
+  except: 
+    return None
 
 def build_rf_list(trees1, trees2):
   rf_list = []
@@ -45,6 +48,9 @@ def build_rf_list(trees1, trees2):
 def analyze_correctness(trees1, trees2, tree2_file, name):
   print("")
   print("## Analysing " + name + " trees...")
+  if (trees2 == None):
+    print("No trees")
+    return
   rf_list = build_rf_list(trees1, trees2)
   rf_average = sum(rf_list) / float(len(rf_list))
   exactness_frequency = rf_list.count(0.0) / float(len(rf_list))
@@ -65,13 +71,17 @@ raxml_trees_file = sys.argv[2]
 treerecs_trees_file = sys.argv[3]
 true_trees = read_list_trees(true_trees_file)
 raxml_trees = read_list_trees(raxml_trees_file)
-treerecs_trees = read_list_trees(treerecs_trees_file)
+treerecs_trees_file_best = treerecs_trees_file + ".best"
+treerecs_trees_file_treesearch = treerecs_trees_file + ".tree_search"
+treerecs_trees_best = read_list_trees(treerecs_trees_file_best)
+treerecs_trees_treesearch = read_list_trees(treerecs_trees_file_treesearch)
 threshold_trees_dir = 0
 if (len(sys.argv) == 5):
   threshold_trees_dir = sys.argv[4]
 
 analyze_correctness(true_trees, raxml_trees, raxml_trees_file, "RAXML")
-analyze_correctness(true_trees, treerecs_trees, treerecs_trees_file, "TREERECS")
+analyze_correctness(true_trees, treerecs_trees_best, treerecs_trees_file_best, "TREERECS BEST THRESHOLD")
+analyze_correctness(true_trees, treerecs_trees_treesearch, treerecs_trees_file_treesearch, "TREERECS TREE SEARCH")
 
 
 if (threshold_trees_dir != 0):
