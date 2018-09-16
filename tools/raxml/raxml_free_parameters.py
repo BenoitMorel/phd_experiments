8929724c19fb666d96b99a5e0511c4cd98186857
BenoitMorel <benomorel@gmail.com>
2018-09-16 21:05:04 +0200

    Do not compute Akaike scores when k > n

--- tools/raxml/raxml_free_parameters.py
+++ tools/raxml/raxml_free_parameters.py
@@ -71,9 +71,14 @@ else:
   print("Unrecognized brlen " + brlen + ". Aborting.")
   sys.exit(1)
 
+sample_size = sites
+if (free_parameters >= sample_size):
+  print("more free parameters than samples! I will not compute the Akaike scores!")
+  sys.exit(0)
+
 AIC = 2.0 * free_parameters - 2.0 * log_likelihood
-BIC = math.log(sites) * free_parameters - 2.0 * log_likelihood
-AICc = AIC + 2.0 * free_parameters * (free_parameters + 1) / (sites - free_parameters - 1) 
+BIC = math.log(sample_size) * free_parameters - 2.0 * log_likelihood
+AICc = AIC + 2.0 * free_parameters * (free_parameters + 1) / (sample_size - free_parameters - 1) 
 
 print("")
 print("Brlen mode: " + brlen)
