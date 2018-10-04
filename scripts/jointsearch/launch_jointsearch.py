46f386dbc2833f1011b28e07c97b8ae829826be7
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2018-10-04 10:05:56 +0200

    Add JointSearch experimental scripts

--- scripts/jointsearch/launch_jointsearch.py
+++ scripts/jointsearch/launch_jointsearch.py
@@ -19,16 +19,23 @@ def get_gene_tree(datadir, tree):
 
 
 def get_tree_search_command(gene_tree, species_tree, alignment, strategy, cores, output_dir):
-    executable = exp.treerecs_joint_search_exec
+    executable = exp.joint_search_exec
     joint_search_output = os.path.join(output_dir, "join_search")
     command = []
     command.append(executable)
+    command.append("-g")
     command.append(gene_tree)
+    command.append("-a")
     command.append(alignment)
+    command.append("-s")
     command.append(species_tree)
+    command.append("--strategy")
     command.append(strategy)
+    command.append("-t")
     command.append(str(cores))
+    command.append("-p")
     command.append(joint_search_output)
+    command.append("--verbose")
     return " ".join(command)
 
 def get_compare_rf_command(data_dir, tree, name, output):
@@ -47,11 +54,11 @@ for dataset in os.listdir(root_datadir):
 
 
 if len(sys.argv) != 6:
-  print("Syntax error: python lauch_treerecs.py dataset strategy starting_tree cluster cores.\n Suggestions of datasets: ")
+  print("Syntax error: python lauch_jointsearch.py dataset strategy starting_tree cluster cores.\n Suggestions of datasets: ")
   for dataset in datasets:
     print("\t" + dataset)
   print("Cluster can be either normal, haswell or magny")
-  print("strategy: SPR or NNI")
+  print("strategy: SPR, NNI, HYBRID")
   print("starting_tree: raxml, true, treerecs, random")
   sys.exit(0)
 
@@ -61,10 +68,10 @@ starting_tree = sys.argv[3]
 cluster = sys.argv[4]
 cores = int(sys.argv[5])
 
-if (not (strategy in ["SPR", "NNI"])):
+if (not (strategy in ["SPR", "NNI", "HYBRID"])):
   print("Unknown search strategy " + strategy)
 
-resultsdir = exp.create_result_dir(os.path.join("treerecs", "joint_search", dataset, strategy + "_start_" + starting_tree, cluster + "_" + str(cores), "run"))
+resultsdir = exp.create_result_dir(os.path.join("JointSearch", dataset, strategy + "_start_" + starting_tree, cluster + "_" + str(cores), "run"))
 result_msg = "Treerecs git: \n" + exp.get_git_info(exp.treerecs_root)
 exp.write_results_info(resultsdir, result_msg) 
 
@@ -79,7 +86,7 @@ output_dir = resultsdir
 
 command = get_tree_search_command(gene_tree, species_tree, alignment, strategy, cores, output_dir)
 
-result_tree = command.split(" ")[-1] + ".newick"
+result_tree = os.path.join(output_dir, "join_search.newick") 
 summary = os.path.join(output_dir, "summary.txt")
 
 
