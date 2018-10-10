316ac5fd02fef5c5e37733224c6861829c5010b0
BenoitMorel <benomorel@gmail.com>
2018-10-10 10:04:00 +0200

    Version the script to extract single gene families from eric's simulated datasets

---
+++ tools/jointsearch/extract_gene_famil.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import shutil
+
+def find_family_index(alignments_info_file, alignment_name):
+  lines = open(alignments_info_file).readlines()[1:]
+  for i in range(0, len(lines)):
+    if "/" + alignment_name in lines[i] or lines[i].startswith(alignment_name):
+      return i
+  return -1
+
+def extract_line(input_file, index, output_file):
+  lines = open(input_file).readlines()
+  with open(output_file, "w") as writer:
+    current_index = 0
+    for line in lines:
+      if (">" in line):
+        continue
+      else:
+        if (current_index == index):
+          writer.write(line)
+          return
+        else:
+          current_index += 1
+
+if (len(sys.argv) != 4):
+  print("Syntax : input_dataset_dir alignment_name output_dir")
+  sys.exit(1)
+
+input_dataset_dir = sys.argv[1]
+alignment_name = sys.argv[2]
+output = sys.argv[3]
+
+species_tree = os.path.join(input_dataset_dir, "speciesTree.newick")
+true_trees = os.path.join(input_dataset_dir, "trueGeneTrees.newick")
+raxml_trees = os.path.join(input_dataset_dir, "geneTrees.newick")
+treerecs_trees = os.path.join(input_dataset_dir, "treerecs_output.newick.best")
+alignments_info_file = os.path.join(input_dataset_dir, "alignment.txt")
+msa_file = os.path.join(input_dataset_dir, "alignments", alignment_name)
+
+output_species_tree = os.path.join(output, "speciesTree.newick")
+output_true_tree = os.path.join(output, "trueGeneTree.newick")
+output_raxml_tree = os.path.join(output, "raxmlGeneTree.newick")
+output_treerecs_tree = os.path.join(output, "treerecsGeneTree.newick")
+output_msa = os.path.join(output, "alignment.msa")
+
+
+os.makedirs(output)
+shutil.copyfile(species_tree, output_species_tree)
+shutil.copyfile(msa_file, output_msa)
+family_index = find_family_index(alignments_info_file, alignment_name)
+print(family_index)
+
+extract_line(true_trees, family_index, output_true_tree)
+extract_line(raxml_trees, family_index, output_raxml_tree)
+extract_line(treerecs_trees, family_index, output_treerecs_tree)
+
+
