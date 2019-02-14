b5b2eedf5d84fc2bede51d25e718eb5ba51f21f1
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2019-02-14 22:08:56 +0100

    Add a script to rescale the BL of a tree

---
+++ tools/trees/rescale_bl.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from ete3 import Tree
+
+def read_tree(tree):
+  lines = open(tree).readlines()
+  for line in lines:
+    if (not line.startswith(">")):
+      return Tree(line, format=1)
+  return None
+
+def rescale_bl(input_tree, output_tree, scale):
+  tree = read_tree(input_tree)
+  for node in tree.traverse("postorder"):
+    node.dist *= scale
+  tree.write(outfile=output_tree)
+
+if (__name__ == "__main__"):
+  if (len(sys.argv) != 4):
+    print("Syntax: python rescale_bl.py input_tree output_tree scale")
+    exit(1)
+  input_tree = sys.argv[1]
+  output_tree = sys.argv[2]
+  scale = float(sys.argv[3])
+  rescale_bl(input_tree, output_tree, scale)
