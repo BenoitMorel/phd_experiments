839bd658268d323d1d68295095292648ade43f7f
BenoitMorel <benomorel@gmail.com>
2019-02-14 11:22:05 +0100

    Update the plots script

--- results/JointSearchPaper/plot_jointsearch_sites.py
+++ results/JointSearchPaper/plot_jointsearch_sites.py
@@ -3,22 +3,25 @@ import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# https://python-graph-gallery.com/122-multiple-lines-chart/
 
 df = pd.DataFrame()
+f, ax = plt.subplots(1)
+ax.set_ylim(ymin=0)
+methods = ["RAxML-NG", "Notung", "Phyldog", "Treerecs", "JointSearch"]
 
-df['x'] = [250, 500, 1000, 1500]
+df['sites'] = [250, 500, 1000, 1500]
 df['RAxML-NG'] = [0.358970396742, 0.281401328493, 0.206761354298, 0.188365444752]
 df['Treerecs'] = [0.258528783379, 0.187579630999, 0.129546138407, 0.119346474143]
 df['Phyldog'] = [0.272231488512, 0.20714293281, 0.131212412268, 0.131669602346]
 df['Notung'] = [0.355859146335, 0.218086125849, 0.168773155905, 0.157012245646]
 df['JointSearch'] = [0.20784454721, 0.165717366227, 0.0997375796799, 0.0911058132789]
 
-print(df)
+for method in methods:
+  plt.plot( 'sites', method, data=df, marker='x', linewidth=2)
 
-# Plot the responses for different events and regions
-ax = sns.lineplot(   data=df)
-ax.set(xlabel="sites", ylabel="average RF distance")
-
-
+plt.xlabel('Sites')
+plt.ylabel('RF distance')
+plt.legend()
 plt.show()
 
