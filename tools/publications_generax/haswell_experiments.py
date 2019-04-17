efc5a56dff2ffb5160d2d2240fa68afb55a4a17f
BenoitMorel <benomorel@gmail.com>
2019-04-17 11:50:30 +0200

    merge

--- tools/publications_generax/haswell_experiments.py
+++ tools/publications_generax/haswell_experiments.py
@@ -20,6 +20,9 @@ if (False):
   
 
 if (True):
+  common.submit_multiple_experiments_haswell(["cyano_simulated"], False, 512)
+
+if (False):
   fixed_point_dl = "jsim_s19_f100_sites500_dna4_bl0.5_d0.25_l0.25_t0.0"
   datasets.append(fixed_point_dl)
   common.add_dataset(datasets, fixed_point_dl, ["d0.01_l0.01", "d0.05_l0.05", "d0.1_l0.1", "d0.4_l0.4"])
@@ -28,7 +31,7 @@ if (True):
   common.add_dataset(datasets, fixed_point_dl, ["bl0.01", "bl0.05", "bl0.1", "bl0.2", "bl1.0", "bl2.0"])
   common.add_dataset(datasets, fixed_point_dl, ["s5", "s10", "s27", "s41"])
 
-if (True):
+if (False):
   fixed_point_dtl = "jsimdtl_s16_f100_sites500_dna4_bl0.5_d0.1_l0.2_t0.1"
   datasets.append(fixed_point_dtl)
   common.add_dataset(datasets, fixed_point_dtl, ["s5", "s10", "s12", "s19", "s27", "s41"])
@@ -37,6 +40,9 @@ if (True):
   common.add_dataset(datasets, fixed_point_dtl, ["d0.01_l0.02_t0.01", "d0.05_l0.1_t0.05", "d0.15_l0.3_t0.15", "d0.2_l0.4_t0.2"])
   common.add_dataset(datasets, fixed_point_dtl, ["d0.01_l0.2_t0.19", "d0.05_l0.2_t0.15", "d0.15_l0.2_t0.05", "d0.19_l0.2_t0.01"])
 
+
 common.submit_multiple_experiments_haswell(datasets, do_generate, cores)
+
+
 
 
