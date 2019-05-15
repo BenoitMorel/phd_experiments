bd0ac36121dc83d0f9fd51c618c3542ee18d5e74
BenoitMorel <benomorel@gmail.com>
2019-05-15 17:30:01 +0100

    add raxml practical test file

--- tools/code/add_std.sh
+++ tools/code/add_std.sh
@@ -1,17 +1,19 @@
-names="ofstream ifstream chrono vector cout cerr endl string min( map exception unordered_map istringstream runtime_error setprecision ostringstream shared_ptr fixed ostream"
-files="core/*/* core/*/*/* JointSearch/* SpeciesRax/* GeneRax/*"
+names="array make_shared ofstream ifstream chrono vector cout cerr endl string map exception unordered_map istringstream runtime_error setprecision ostringstream shared_ptr fixed ostream stack min( ios stringstream istreambuf_iterator numerical_limits unordered_set isnormal isnan numeric_limits hash"
+files="core/* core/*/* core/*/*/* JointSearch/* SpeciesRax/* GeneRax/*"
 for file in $files
 do
   echo $file
   sed -i "s/using namespace std\;//g" $file
   for name in $names
   do
-    echo $name
-    sed -i "s/$name\([^A-Za-z0-9]\)/std::$name\1/g" $file
+    sed -i "s/$name\([^A-Za-z0-9_]\)/std::$name\1/g" $file
+    sed -i "s/$name$/std::$name/g" $file
   done
   sed -i "s/std::std::/std::/g" $file
   sed -i "s/\([A-Za-z0-9_\.][A-Za-z0-9_\.]*\)std::/\1/g" $file
   sed -i "s/unordered_map/std::unordered_map/g" $file
+  sed -i "s/min(/std::min(/g" $file
+  sed -i "s/max(/std::max(/g" $file
   sed -i "s/std::std::/std::/g" $file
   sed -i "s/include \([<\"]\)std::/include\ \1/g" $file
 done 
