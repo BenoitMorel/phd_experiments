f0dc6526694e6219f0657748481a05e1f9e9aebe
BenoitMorel <benomorel@gmail.com>
2019-05-24 17:52:22 +0100

    finish the script to perturbate a rooted tree

--- tools/trees/perturbate.py
+++ tools/trees/perturbate.py
@@ -1,5 +1,6 @@
 from ete3 import Tree
 import random
+import sys
 
 def get_random_node(tree):
     l = []
@@ -8,23 +9,43 @@ def get_random_node(tree):
     rdm = random.randint(1,1000000)
     return l[rdm % len(l)]
     
-def perturbate(tree):
-    
+def get_random_son(node):
+    rdm = random.randint(1, 1000000)
+    return node.get_children()[rdm % len(node.get_children())]
+
+def perturbate_tree(tree):
     node = get_random_node(tree)
     if (node.is_root()):
         return
-    sister = node.get_sisters()[0]
+    parent_swap = node.get_sisters()[0]
+    if (parent_swap.is_leaf()):
+        return
+    parent_node = node.get_ancestors()[0]
+    swap = get_random_son(parent_swap) 
     node.detach()
-    sister.add_child(node)
+    swap.detach()
+    parent_swap.add_child(node)
+    parent_node.add_child(swap)
 
+def perturbate(input_tree_path, output_tree_path, min_aRF):
+    original_tree = Tree(open(input_tree_path).read(), format=1)
+    tree = Tree(open(input_tree_path).read(), format=1)
+    for i in range(0, 10000):
+        perturbate_tree(tree)
+        RF = tree.robinson_foulds(original_tree, unrooted_trees=True)
+        aRF = float(RF[0]) / float(RF[1])
+        print("RF: " + str(RF[0]) + " " + str(aRF))
+        if (aRF >= min_aRF):
+            break
+    print("Saving perturbated tree in " + output_tree_path)
+    open(output_tree_path, "w").write(tree.write())
 
-species_tree = open("../BenoitDatasets/families/swiss/speciesTree.newick").read()
-tree1 = Tree(species_tree, format=1) 
-tree2 = Tree(species_tree, format=1) 
-
-print(tree1.robinson_foulds(tree2, unrooted_trees = True)[0])
-for i in range(0, 100):
-    perturbate(tree1)
-    print(tree1.robinson_foulds(tree2, unrooted_trees = True)[0])
-
-
+if (__name__ == "__main__"):
+    if (len(sys.argv) != 4):
+        print("Syntax: input_tree_path output_tree_path min_aRF")
+        exit(1)
+    input_tree_path = sys.argv[1]
+    output_tree_path = sys.argv[2]
+    min_aRF = float(sys.argv[3])
+    perturbate(input_tree_path, output_tree_path, min_aRF)
+    
