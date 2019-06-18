3fbb8544cbe4b24e8b5d6baf2a2be51f7a843cc3
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2019-06-18 16:16:35 +0200

    Fix JC model

--- tools/families/sequence_model.py
+++ tools/families/sequence_model.py
@@ -23,6 +23,8 @@ def get_phyldog_model(subst_model):
 def get_mrbayes_preset_line(subst_model):
   if (get_model(subst_model) == "LG"):
     return "\tprset aamodelpr=fixed(lg);\n"
+  elif(get_model(subst_model) == "JC"):
+    return "\tprset statefreqpr=fixed(equal);\n"
   else:
     return ""
 
@@ -30,11 +32,11 @@ def get_mrbayes_lset_line(subst_model):
   model = get_model(subst_model)
   line = "\t"
   rates = get_gamma_rates(subst_model)
-  line += "lset nst="
-  if (model == "GTR" or model == "LG"):
+  line += "lset "
+  if (model == "nst=GTR"):
     line += "6"
   elif (model == "JC"):
-    line += "2"
+    line += "nst=2"
   else:
     assert(False)
   if (rates == 1):
