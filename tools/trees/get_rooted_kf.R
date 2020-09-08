3273c05743db21bee1ffd9ac70a557bed72f6478
BenoitMorel <benomorel@gmail.com>
2020-09-08 22:46:06 +0200

    fix  get_rooted_kf

--- tools/trees/get_rooted_kf.R
+++ tools/trees/get_rooted_kf.R
@@ -6,12 +6,12 @@ main <- function(tree1_path, tree2_path)
 {
   tree1 <- read.tree(tree1_path)
   tree2 <- read.tree(tree2_path)
-  run_kf <- mean(KF.dist(tree1, tree2))
+  run_kf <- mean(KF.dist(tree1, tree2, rooted=TRUE))
   print(run_kf)
 }
 
 args = commandArgs(trailingOnly=TRUE)
 tree1_path <- args[1]
 tree2_path <- args[2]
-main(tree1, tree2)
+main(tree1_path, tree2_path)
 
--- tools/trees/get_rooted_kf.py
+++ tools/trees/get_rooted_kf.py
@@ -9,13 +9,16 @@ import subprocess
 def get_rooted_kf(tree1, tree2):
     command = []
     command.append("R")
+    command.append("--slave")
     command.append("-f")
     command.append(exp.getrootedkf_R_script)
     command.append("--args")
     command.append(tree1)
-    command.append(tree1)
-    subprocess.check_call(command)
-
+    command.append(tree2)
+    print(" ".join(command))
+    out = subprocess.check_output(command)
+    kf_distance = float(out.split()[1])
+    print(kf_distance)
 
 if __name__ == '__main__':
     if (len(sys.argv) != 3):
@@ -23,5 +26,6 @@ if __name__ == '__main__':
         sys.exit(1)
     tree1 = sys.argv[1]
     tree2 = sys.argv[2]
-    get_rooted_kf(tree1, tree1)
+    for i in range(0, 10):
+      get_rooted_kf(tree1, tree2)
     
