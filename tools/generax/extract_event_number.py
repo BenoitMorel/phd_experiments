021d07c2c2f532c73085091c12dc8e6922ed165e
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2021-01-04 22:48:37 +0100

    make extract_event_number faster

--- tools/generax/extract_event_number.py
+++ tools/generax/extract_event_number.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import glob
+from ete3 import Tree
 
-
+def get_species_leaves(generaxdir):
+  species_tree_file = os.path.join(generaxdir, "species_trees", "starting_species_tree.newick")
+  tree = Tree(species_tree_file, format = 1)
+  return set(tree.get_leaf_names())
 
 def extract(generaxdir):
   events = {}
@@ -11,15 +15,22 @@ def extract(generaxdir):
   events["D"] = 0
   events["T"] = 0
   events["TL"] = 0
-  events["L"] = 0
   events["Leaf"] = 0
-  pattern = os.path.join(generaxdir, "reconciliations", "*_eventCounts.txt")
-  event_count_files = glob.glob(pattern)
-  for f in event_count_files:
-    lines = open(f).readlines()
-    for line in lines:
-      sp = line.replace("\n", "").split(":")
-      events[sp[0]] = events[sp[0]] + int(sp[1])
+  f = os.path.join(generaxdir, "per_species_event_counts.txt")
+  
+  species = get_species_leaves(generaxdir)
+  lines = open(f).readlines()
+  for line in lines:
+    if (line[0] == "#"):
+      continue
+    sp = line.replace("\n", "").split(" ")
+    is_leaf = sp[0] in species
+    for event in sp[1:]:
+      sp2 = event.split("=")
+      key = sp2[0]
+      if (key == "S" and is_leaf):
+        key = "Leaf"
+      events[key] += int(sp2[1])
   return events
 
 def run(generaxdir):
