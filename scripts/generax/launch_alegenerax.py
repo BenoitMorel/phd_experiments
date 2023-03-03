37cee20792ed998c20388540854b8b92773a3751
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2023-03-03 15:52:30 +0100

    taxonomic renaming

--- scripts/generax/launch_alegenerax.py
+++ scripts/generax/launch_alegenerax.py
@@ -105,7 +105,10 @@ def get_alegenerax_command(alegenerax_families_file, starting_species_tree, addi
     command.append("--species-search")
     command.append("EVAL")
     command.append("--min-covered-species")
-    command.append("1")
+    if ("--prune-species-tree" in additional_arguments):
+      command.append("4")
+    else:
+      command.append("1")
     command.append("--gene-tree-samples")
     command.append("1")
     command.extend(additional_arguments)
